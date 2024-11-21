952bf489108956a6658cdc019fc0ac08e4a9eee9
Alexander <mightee.cactus@gmail.com>
2024-11-21 22:40:12 +0300

    реализация через Hibernate 5; добавил аннотации в User

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,132 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
-
-    }
-
+    public UserDaoHibernateImpl() {}
 
     @Override
     public void createUsersTable() {
-
+        String q = "create table USERS" +
+                " (id integer primary key autoincrement," +
+                " name varchar(255) not null," +
+                " last_name varchar(255) not null," +
+                " age integer not NULL" +
+                ");";
+        executeNativeSql(q);
     }
 
     @Override
     public void dropUsersTable() {
-
+        String q = "drop table if exists USERS;";
+        executeNativeSql(q);
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Session s = Util.getSession();
+        Transaction tx = null;
+        User user = new User(name, lastName, age);
 
+        try {
+            tx = s.beginTransaction();
+            s.save(user);
+            tx.commit();
+        }
+        catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            s.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Session s = Util.getSession();
+        Transaction tx = null;
 
+        try {
+            User user = s.get(User.class, id);
+            if (user == null) return;
+
+            tx = s.beginTransaction();
+            s.remove(user);
+            tx.commit();
+        }
+        catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            s.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        Session s = Util.getSession();
+
+        try {
+            return s.createQuery("from User", User.class).list();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            s.close();
+        }
+
+        return new ArrayList<>();
     }
 
     @Override
     public void cleanUsersTable() {
+        Session s = Util.getSession();
+        Transaction tx = null;
 
+        try {
+            tx = s.beginTransaction();
+            s.createQuery("delete from User").executeUpdate();
+            tx.commit();
+        }
+        catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            s.close();
+        }
+    }
+
+    /**
+     * Reducing copypaste :)
+     * @param query string
+     * @return count of affected rows
+     */
+    private int executeNativeSql(String query)
+    {
+        Session s = Util.getSession();
+        Transaction tx = null;
+
+        int affected = 0;
+
+        try {
+            tx = s.beginTransaction();
+            affected = s.createSQLQuery(query).executeUpdate();
+            tx.commit();
+        }
+        catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            s.close();
+        }
+
+        return affected;
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     private static Connection conn;
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() throws SQLException {
         if (conn != null) {
@@ -16,11 +22,31 @@ public class Util {
         return conn;
     }
 
-    public static void closeConnection(){
+    public static void closeConnection() {
         if (conn == null) return;
         try {
             conn.close();
         } catch (SQLException ignored) {
         }
+    }
+
+    public static Session getSession() {
+        if (sessionFactory == null) {
+            sessionFactory = createSessionFactory();
+        }
+
+        return sessionFactory.openSession();
+    }
+
+    private static SessionFactory createSessionFactory() {
+        Configuration config = new Configuration();
+
+        config.setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC")
+                .setProperty("hibernate.connection.url", "jdbc:sqlite:my_awesome_database.db")
+                .setProperty("hibernate.dialect", "org.sqlite.hibernate.dialect.SQLiteDialect")
+                .setProperty("hibernate.hbm2ddl", "create-drop")
+                .addAnnotatedClass(User.class);
+
+        return config.buildSessionFactory();
     }
 }
